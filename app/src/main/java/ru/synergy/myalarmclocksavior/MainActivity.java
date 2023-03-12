e6926c8855fa2ab5984b27efa78ad5809ec5434f
bystrovdev <bystrovdev@mail.ru>
2023-03-12 19:13:36 +0400

    First screen layout

--- app/src/main/java/ru/synergy/myalarmclocksavior/MainActivity.java
+++ app/src/main/java/ru/synergy/myalarmclocksavior/MainActivity.java
@@ -3,12 +3,22 @@ package ru.synergy.myalarmclocksavior;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button setAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
